4379071f9d5685663a336e52c76151fea29a9558
ikovalev <ikovalev@lanit.ru>
2021-01-03 23:24:10 +0300

    start

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
+
+import {AppRoutingModule} from './app-routing.module'
+import {AppComponent} from './app.component'
+import {PhotosComponent} from './photos/photos.component'
+
+@NgModule({
+  declarations: [AppComponent, PhotosComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
