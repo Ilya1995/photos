fbb466b0ea48632218cba10161145e2690ec0ecc
ikovalev <ikovalev@lanit.ru>
2021-03-03 00:03:31 +0300

    github.io

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCCbpJIglbgAP6CNo48NBZPPbG7p9B9egg',
+    authDomain: 'photos-3bd12.firebaseapp.com',
+    databaseURL: 'https://photos-3bd12-default-rtdb.firebaseio.com',
+    projectId: 'photos-3bd12',
+    storageBucket: 'photos-3bd12.appspot.com',
+  },
+}
