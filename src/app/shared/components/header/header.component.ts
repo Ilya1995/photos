a6c1da8c753573c5071ea97a37e4356996eb206c
ikovalev <ikovalev@lanit.ru>
2021-03-12 00:25:01 +0300

    fixed header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core'
+import {Component, AfterViewInit, OnDestroy, ViewChild} from '@angular/core'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements AfterViewInit, OnDestroy {
 
-  constructor() { }
+  @ViewChild('sticky') sticky
 
-  ngOnInit() {
+  fixed = false
+  emptyHeight = 0
+  neededScroll = 0
+  constructor() {}
+
+  ngAfterViewInit() {
+    this.emptyHeight = this.sticky.nativeElement.offsetHeight
+    this.neededScroll = this.sticky.nativeElement.offsetTop - (100 - this.sticky.nativeElement.offsetHeight) / 2
+    window.addEventListener('scroll', this.handleScroll)
   }
 
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = () => {
+    this.fixed = window.scrollY > this.neededScroll
+  }
 }
